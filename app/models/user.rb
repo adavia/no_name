e31a4efe6aaf6507eb45600576ab6902f77c0afd
adavia <andresi.davia@gmail.com>
2017-06-10 17:06:45 -0500

    Add rule validation to user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, if: :password
-  validates :role, presence: true
+  validates :role, presence: true, unless: Proc.new { |a| a.admin? }
 
   has_secure_password
 
